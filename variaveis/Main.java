0e336a6f9f47283a8555247bc4046b1ea071d4b0
Marcos Júnior Lemes ferreira <marcos.jlf@aluno.ifsc.edu.br>
2025-04-28 12:43:36 -0300

    Study about variables and types of data
    A short study about variables, types of data, and arrays.

---
+++ variaveis/Main.java
@@ -0,0 +1,30 @@
+package variaveis;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // tios primitivos
+
+        int number01 = 1;
+
+        short number02 = 2;
+
+        long number03 = 3L;
+
+        float number04 = 4f;
+
+        double number05 = 5d;
+
+        boolean on = true;
+
+        char number06 = 'C';
+
+        String nome = "marcos";
+
+
+        System.out.println("Esse são alguns tipos de dados, nomeado como tipos primitivos:");
+        System.out.println("Int: "+number01+" short: "+ number02+" long: "+ number03+" float: "+number04+ " double: "+ number05+ " boolean: "+ on+ " char: "+number06+" String: "+nome);
+    }
+    
+}
